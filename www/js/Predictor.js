f09ce2884044c30489654f327ba116768deb4715
Selino <selino@selino.com>
2014-01-29 08:28:08 -0800

    Code cleanup.

--- www/js/Predictor.js
+++ www/js/Predictor.js
@@ -11,19 +11,19 @@ var Predictor = {
         var myTime = new Date(predictionData.predictDate).getTime();
         
         if(myTime <= currentTime) {
-            $('#prediction-text').html("Please choose a date in the future.");
+            $("#prediction-text").html("Please choose a date in the future.");
         } else {
-            $('#prediction-text').html(myFlo.name + " will be " + myPrediction.text + " on " + humanDate(predictionData.predictDate) + ".");
+            $("#prediction-text").html(myFlo.name + " will be " + myPrediction.text + " on " + humanDate(predictionData.predictDate) + ".");
         }
         
-        $('#popupDialog').popup('open');
+        $("#popupDialog").popup('open');
     }
 }
 
 $("#predict-page").on('pagebeforeshow', function(evt) {
                       var myDate = new Date();
                       myDate.setDate(myDate.getDate() + 1);
-                      $('#predictDate').val(myDate.toJSON().slice(0,10));
+                      $("#predictDate").val(myDate.toJSON().slice(0,10));
                       });
 
 $("#predict-button").on('tap', function(evt){
