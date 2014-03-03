5d733a64bbd3ff60dfd7d0a92c4088a9b4eb2056
Selino <selino@selino.com>
2014-03-02 17:42:42 -0800

    Refactoring with JSLint. Dear, Lord!

--- www/js/App.js
+++ www/js/App.js
@@ -1,104 +1,40 @@
-lowLag.init({
-            'urlPrefix':'audio/',
-            'debug':'none'
-            });
-lowLag.load("button-31.mp3",'btn-snd-1');
-lowLag.load("button-32.mp3",'btn-snd-2');
-lowLag.load("button-43.mp3",'btn-snd-3');
+"use strict";
 
-$('[data-id=header] a,.ui-btn').on('click', function(){
-                 lowLag.play('btn-snd-2');
-                 });
-
-$("#list-page").on('pagebeforeshow', function(evt) {
-                   makeList();
-                   });
-
-$("#edit-page").on('pageinit', function (evt) {
-                   checkNameDateFields();
-                   
-                   }).on('pagebeforehide', function(evt) {
-                         updateEditPage();
-                         }).on('pagebeforeshow', function(evt) {
-                               updateEditPage();
-                               $("#edit-form").show();
-                               }).on('swiperight', swipeEditPage);
-
-$("#delete-button").on('click', function() {
-                           var deleteName = $("#name").val();
-                           $("#delete-popup-name").html(deleteName)
-                           $("#popup-delete").popup('open');
-                       });
-
-$("#list-page-button").on('click', function() {
-                       $.mobile.changePage( '#list-page', { transition: 'slide', reverse: true});
-                       });
-
-$("#delete-popup-confirm-btn").on('click', function() {
-                                  startDeleteFlo();
-                                  });
-
-$(".thumbnail-button").on('click', function() {
-                          lowLag.play('btn-snd-1');
-                          getPhoto(pictureSource.PHOTOLIBRARY);
-                          });
-
-$("input[type=text]").on("keypress", function(e) {
-                         if(e.which === 13) {
-                             this.blur();
-                         }
-                         });
-
-$("#add-flo-btn").on('click', function() {
-                     createNewFlo();
-                     });
-
-$(document).on('slidestart', '#edit-form input[type=number]', function(){
-                   $("#edit-page").unbind('swiperight');
-               }).on('slidestop', '#edit-form input[type=number]', function(){
-                     $("#edit-page").on('swiperight', swipeEditPage);
-               });
-
-function swipeEditPage(){
-    $.mobile.changePage( '#list-page', { transition: 'slide', reverse: true});
-    lowLag.play('btn-snd-3');
-}
-
-function onResume(){
-    var activePage = $.mobile.activePage.attr("id");
-    if (activePage == "list-page") {
-        makeList();
-    }
-}
-
-function startDeleteFlo(){
-    $("#edit-form").fadeOut('fast', function(){
-                            deleteFloThumbnail();
-                            Flos.deleteFlo(gCurrentFlo);
-                            $.mobile.changePage( '#list-page');
-                            });
-}
-
-function deleteFloThumbnail(){
+function deleteFloThumbnail() {
     var sThumbnailPath = $('#thumbnail').val();
     
-    if (sThumbnailPath != 'images/thumbnail.svg') {
-        sThumbnailPath = 'file://' + sThumbnailPath.slice(0, - 24);
+    if (sThumbnailPath !== 'images/thumbnail.svg') {
+        sThumbnailPath = 'file://' + sThumbnailPath.slice(0, -24);
         FileIO.removeDeletedImage(sThumbnailPath);
     }
 }
 
-function displayInsertResult(tx, rs){
+function startDeleteFlo() {
+    $("#edit-form").fadeOut('fast', function () {
+        deleteFloThumbnail();
+        Flos.deleteFlo(gCurrentFlo);
+        $.mobile.changePage('#list-page');
+    });
+}
+
+function displayInsertResult(tx, rs) {
     gCurrentFlo = rs.insertId;
 }
 
-function createNewFlo(){
+function resetEditPage() {
+    $('#edit-form').resetForm();
+    $('#thumbnail').val('images/thumbnail.svg');
+    $('.thumbnail-button').attr('src', 'images/thumbnail.svg');
+    $('#startDate').val(new Date().toJSON().slice(0, 10));
+}
+
+function createNewFlo() {
     resetEditPage();
     var myNewFlo = $('#edit-form').stringifyForm();
     Model.addFlo(myNewFlo);
 }
 
-function populateEditPage(e){
+function populateEditPage(e) {
 	var row = globalFlos[e];
 	gCurrentFlo = row.id;
 	
@@ -107,7 +43,7 @@ function populateEditPage(e){
 	$('#long').val(row.long).trigger('create');
 	$('#startDate').val(row.startDate);
     $('#thumbnail').val(row.thumbnail);
-    $('.thumbnail-button').attr('src',row.thumbnail).nocacheImg();
+    $('.thumbnail-button').attr('src', row.thumbnail).nocacheImg();
 }
 
 function updateEditPage() {
@@ -115,59 +51,49 @@ function updateEditPage() {
     Model.updateFlo(myFloEdit);
 }
 
-function makeList(){
+function makeList() {
 	Model.openDB();
 	Model.getFlos();
     //resetEditPage();
 }
 
-function resetEditPage(){
-    $('#edit-form').resetForm();
-    $('#thumbnail').val('images/thumbnail.svg');
-    $('.thumbnail-button').attr('src','images/thumbnail.svg');
-    $('#startDate').val(new Date().toJSON().slice(0,10));
-}
-
 function checkNameDateFields() {
-    $('#edit-form #startDate').on('blur', function() {
-                                  var dateValue = this.value;
-                                  var bIsPast = makeSurePast(dateValue);
-                                  
-                                  if (!bIsPast) {
-                                      $('#startDate').val(new Date().toJSON().slice(0,10));
-                                  }
-                                  
-                                  document.body.scrollTop = document.documentElement.scrollTop = 0;
-                                  })
+    $('#edit-form #startDate').on('blur', function () {
+        var dateValue = this.value, bIsPast = makeSurePast(dateValue);
+        if (!bIsPast) {
+            $('#startDate').val(new Date().toJSON().slice(0, 10));
+        }
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
     
-    $('#edit-form #name').on('blur', function() {
-                             var nameValue = this.value;
-                             if (nameValue == '' || nameValue == null) {
-                                 $('#name').val('Unnamed');
-                             }
-                             document.body.scrollTop = document.documentElement.scrollTop = 0;
-                             })
+    $('#edit-form #name').on('blur', function () {
+        var nameValue = this.value;
+        if (nameValue === '' || nameValue === null) {
+            $('#name').val('Unnamed');
+        }
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+    });
 }
 
-function displayResultSet(){
+function displayResultSet() {
 	var myOutput = '';
     var myPredictOutput = '';
     
     if (globalFlos.length >= 1) {
-        for(i=0; i < globalFlos.length; i++) {
+        for (i = 0; i < globalFlos.length; i += 1) {
             var row = globalFlos[i];
-            var myFloStatus = Flos.getStatus(currentTime,row.startDate,row.cycle,row.long);
+            var myFloStatus = Flos.getStatus(currentTime, row.startDate, row.cycle, row.long);
             
             myOutput += "<div class='edit-button " + myFloStatus.code +
-            "' href='#edit-page' data-id='" + i +
-            "' data-role='button' data-transition='slide' data-shadow='false'>" +
+                "' href='#edit-page' data-id='" + i +
+                "' data-role='button' data-transition='slide' data-shadow='false'>" +
 //            "<div class='crtl-area'>" +
 //            "<a href='#predict-page' data-icon='calendar' data-role='button' data-shadow='false' data-mini='true'>Predict</a>" +
 //            "</div>" +
-            "<div class='info-area snd-one'><image width='60' class='thumbnail' src='"+ row.thumbnail +"' />" +
-            "<image class='carat' src='css/images/icons-svg/carat-l-black.svg' />" +
-            "<span class='name-title'>" + truncateString(row.name, 13) + "</span>" +
-            " <span class='status-text'>is "+ myFloStatus.text +"</span></div></div>";
+                "<div class='info-area snd-one'><image width='60' class='thumbnail' src='" + row.thumbnail + "' />" +
+                "<image class='carat' src='css/images/icons-svg/carat-l-black.svg' />" +
+                "<span class='name-title'>" + truncateString(row.name, 13) + "</span>" +
+                " <span class='status-text'>is " + myFloStatus.text + "</span></div></div>";
             
             myPredictOutput += '<label><input type="radio" name="predictItem" id="pred-item-' + i + '" value="' + i + '" checked>' + row.name + '</label>';
         }
@@ -176,49 +102,49 @@ function displayResultSet(){
         myPredictOutput = myOutput;
     }
     checkForMaxFlos();
-    updateFloListLayout(myOutput,myPredictOutput);
+    updateFloListLayout(myOutput, myPredictOutput);
 }
 
-function setEditBtnActions(){
-    $('.edit-button').on('click', function(event){
-                         lowLag.play('btn-snd-1');
-                         var that = $(this);
-                         setEditBtnTap(that);
-                         }).on('swipeleft', function(event){
-                               $(this).addClass('ui-btn-active');
-                               populateEditPage(
-                                                $(this).attr('data-id')
-                                                );
-                               $.mobile.changePage('#edit-page', { transition: 'slide' });
-                               lowLag.play('btn-snd-3');
-                               }).on('taphold', function(event){
-                                    var oThat = $(this);
-                                    oThat.off('click');
-//                                    showCntrlArea(oThat);
-                                    $(this).on("tap", function(){
-                                               var that = $(this);
-                                               setEditBtnTap(that);
-                                               });
-                                     });
+function setEditBtnActions() {
+    $('.edit-button').on('click', function (event) {
+        lowLag.play('btn-snd-1');
+        var that = $(this);
+        setEditBtnTap(that);
+    }).on('swipeleft', function (event) {
+        $(this).addClass('ui-btn-active');
+        populateEditPage(
+            $(this).attr('data-id')
+        );
+        $.mobile.changePage('#edit-page', { transition: 'slide' });
+        lowLag.play('btn-snd-3');
+    }).on('taphold', function (event) {
+        var oThat = $(this);
+        oThat.off('click');
+        //showCntrlArea(oThat);
+        $(this).on("tap", function () {
+            var that = $(this);
+            setEditBtnTap(that);
+        });
+    });
 }
 
-function showCntrlArea(oEditItem){
-    oEditItem.children( ".crtl-area").slideToggle('fast');
+function showCntrlArea(oEditItem) {
+    oEditItem.children(".crtl-area").slideToggle('fast');
 }
 
-function setEditBtnTap(e){
-    populateEditPage( $(e).attr('data-id') );
+function setEditBtnTap(e) {
+    populateEditPage($(e).attr('data-id'));
     $.mobile.changePage('#edit-page', { transition: 'slide' });
 }
 
-function updateFloListLayout(myOutput,myPredictOutput){
+function updateFloListLayout(myOutput,myPredictOutput) {
     $('#flos-list').html(myOutput);
     $('#predict-list').html(myPredictOutput);
 	setEditBtnActions();
     $('.thumbnail').nocacheImg();
 }
 
-function checkForMaxFlos(){
+function checkForMaxFlos() {
     if (globalFlos.length >= 10) {
         $('#add-flo-btn').hide();
     } else {
@@ -228,25 +154,25 @@ function checkForMaxFlos(){
 
 function updateImageSrc(path) {
     $('#thumbnail').val(path);
-    $('.thumbnail-button').attr('src',path).nocacheImg();
+    $('.thumbnail-button').attr('src', path).nocacheImg();
 }
 
 //primitive helpers
 function secToDays(value) {
-    var d = value/1000/60/60/24;
+    var d = value / 1000 / 60 / 60 / 24;
     return Math.round(d);
 }
 
-function truncateString(string,max){
-    if(string.length >= max){
-        return string.slice(0,max) + "…";
+function truncateString(string, max) {
+    if (string.length >= max) {
+        return string.slice(0, max) + "…";
     } else {
         return string;
     }
 }
 
-function makeSurePast(date){
-    if (date != '' && date != null) {
+function makeSurePast(date) {
+    if (date !== '' && date !== null) {
         var myTime = new Date(date).getTime();
         if (myTime >= currentTime) {
             return false;
@@ -258,8 +184,8 @@ function makeSurePast(date){
     }
 }
 
-function makeSureFuture(date){
-    if (date != '' && date != null) {
+function makeSureFuture(date) {
+    if (date !== '' && date !== null) {
         var myTime = new Date(date).getTime();
         if (myTime <= currentTime) {
             return false;
@@ -271,17 +197,31 @@ function makeSureFuture(date){
     }
 }
 
+function swipeEditPage() {
+    if ($('#edit-page').hasClass('swap-off') === false) {
+        $.mobile.changePage('#list-page', {transition: 'slide', reverse: true});
+        lowLag.play('btn-snd-3');
+    }
+}
+
+function onResume() {
+    var activePage = $.mobile.activePage.attr("id");
+    if (activePage === "list-page") {
+        makeList();
+    }
+}
+
 //JQuery helpers
 $.fn.extend({
     nocacheImg : function(){
         return this.each(function(){
-                         var myNow = new Date().getTime();
-                         this.src = this.src + "?localtime=" + myNow;
-                         });
+            var myNow = new Date().getTime();
+            this.src = this.src + "?localtime=" + myNow;
+        });
     },
             
     resetForm : function(){
-            this[0].reset();
+        this[0].reset();
     },
             
     stringifyForm : function(){
@@ -290,18 +230,78 @@ $.fn.extend({
     },
     
     serializeForm : function(){
-            var o = {};
-            var a = this.serializeArray();
-            $.each(a, function() {
-                   if (o[this.name] !== undefined) {
-                   if (!o[this.name].push) {
-                   o[this.name] = [o[this.name]];
-                   }
-                   o[this.name].push(this.value || '');
-                   } else {
-                   o[this.name] = this.value || '';
-                   }
-                   });
-            return o;
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
     }
+});
+
+lowLag.init({
+    'urlPrefix': 'audio/',
+    'debug': 'none'
+});
+lowLag.load("button-31.mp3", 'btn-snd-1');
+lowLag.load("button-32.mp3", 'btn-snd-2');
+lowLag.load("button-43.mp3", 'btn-snd-3');
+
+$('[data-id=header] a,.ui-btn').on('click', function () {
+    lowLag.play('btn-snd-2');
+});
+
+$("#list-page").on('pagebeforeshow', function (evt) {
+    makeList();
+});
+
+$("#edit-page").on('pageinit', function (evt) {
+    checkNameDateFields();
+}).on('pagebeforehide', function(evt) {
+    updateEditPage();
+}).on('pagebeforeshow', function(evt) {
+    updateEditPage();
+    $("#edit-form").show();
+}).on('swiperight', swipeEditPage);
+
+$("#delete-button").on('click', function() {
+    var deleteName = $("#name").val();
+    $("#delete-popup-name").html(deleteName)
+    $("#popup-delete").popup('open');
+});
+
+$("#list-page-button").on('click', function() {
+    $.mobile.changePage( '#list-page', { transition: 'slide', reverse: true});
+});
+
+$("#delete-popup-confirm-btn").on('click', function() {
+    startDeleteFlo();
+});
+
+$(".thumbnail-button").on('click', function() {
+    lowLag.play('btn-snd-1');
+    getPhoto(pictureSource.PHOTOLIBRARY);
+});
+
+$("input[type=text]").on("keypress", function(e) {
+    if(e.which === 13) {
+        this.blur();
+    }
+});
+
+$("#add-flo-btn").on('click', function() {
+    createNewFlo();
+});
+
+$('#edit-page').on('slidestart', '#edit-form input[type=number]', function() {
+    $("#edit-page").addClass('swap-off');
+}).on('slidestop', '#edit-form input[type=number]', function() {
+    $("#edit-page").removeClass('swap-off');
 });
--- www/js/Predictor.js
+++ www/js/Predictor.js
@@ -1,50 +1,41 @@
 var Predictor = {
-    
-	runPrediction : function () {
-        var predictionData = $('#predict-form').serializeForm();
-        var myFlo = globalFlos[predictionData.predictItem];
-        var myPrediction = Flos.getStatus(predictionData.predictDate,myFlo.startDate,myFlo.cycle,myFlo.long);
-        Predictor.reportPrediction(predictionData,myFlo,myPrediction);
-	},
-    
-    reportPrediction : function(predictionData,myFlo,myPrediction) {
+    runPrediction : function () {
+        var predictionData = $('#predict-form').serializeForm(), myFlo = globalFlos[predictionData.predictItem], myPrediction = Flos.getStatus(predictionData.predictDate, myFlo.startDate, myFlo.cycle, myFlo.long);
+        Predictor.reportPrediction(predictionData, myFlo, myPrediction);
+    },
+    reportPrediction : function (predictionData, myFlo, myPrediction) {
         //var myTime = new Date(predictionData.predictDate).getTime();
-        var newDateFormat = moment(predictionData.predictDate).format('MMMM Do, YYYY');
-        var bIsFuture = makeSureFuture(predictionData.predictDate);
-        
-        if(!bIsFuture) {
+        var newDateFormat = moment(predictionData.predictDate).format('MMMM Do, YYYY'), bIsFuture = makeSureFuture(predictionData.predictDate);
+        if (!bIsFuture) {
             $("#prediction-text").html("Please choose a date in the future.");
         } else {
             $("#prediction-text").html(myFlo.name + " will be " + myPrediction.text + " on " + newDateFormat + ".");
         }
-        
+
         $("#popupDialog").popup('open');
     },
-    
-    getNextDay : function() {
+
+    getNextDay : function () {
         var myDate = new Date();
         myDate.setDate(myDate.getDate() + 1);
         //return myDate;
-        $("#predictDate").val(myDate.toJSON().slice(0,10));
-        
+        $("#predictDate").val(myDate.toJSON().slice(0, 10));
+
     }
-}
+};
 
-$("#predict-page").on('pagebeforeshow', function(evt) {
-                      Predictor.getNextDay();
-                      });
+$("#predict-page").on('pagebeforeshow', function (evt) {
+    Predictor.getNextDay();
+});
 
-$("#predict-button").on('tap', function(evt){
-                        Predictor.runPrediction();
-                        });
+$("#predict-button").on('tap', function (evt) {
+    Predictor.runPrediction();
+});
 
-$("#predict-form #predictDate").on('blur', function() {
-                              var dateValue = this.value;
-                              var bIsFuture = makeSureFuture(dateValue);
-                              
-                              if (!bIsFuture) {
-                                  Predictor.getNextDay();
-                              }
-                              
-                              document.body.scrollTop = document.documentElement.scrollTop = 0;
-                              })
+$("#predict-form #predictDate").on('blur', function () {
+    var dateValue = this.value, bIsFuture = makeSureFuture(dateValue);
+    if (!bIsFuture) {
+        Predictor.getNextDay();
+    }
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+});
